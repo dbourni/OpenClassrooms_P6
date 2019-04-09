84e327f25c1762e91f67bfb789052a6482a3c0c3
David Bournisien <david@bournisien.net>
2019-04-09 13:04:53 +0200

    Login form and users fixtures added

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ */
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return new Response(
+            '<html><body>Homepage.</body></html>'
+        );
+    }
+}
